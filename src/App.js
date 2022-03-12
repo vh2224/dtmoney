165d04011f992314914d4648f4cbe0fdb657eac9
Vinicius Almeida <serradev.viniciusalmeida@cma.com.br>
2022-03-12 16:57:27 -0300

    feat: adicionado modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-//import styled from 'styled-components';
-
+import { useState } from 'react';
 import { Cards } from "./components/Cards/cards";
 import { Header } from "./components/Header/header";
 import { Transaction } from "./components/Transactions";
 import { GlobalStyles } from "./styles/global";
+import Modal from 'react-modal';
+
+
+Modal.setAppElement('#root');
 
 function App() {
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+    function openModal() {
+      setIsOpen(true);
+    }
+
+    function closeModal() {
+      setIsOpen(false);
+    }
+
+
+
   return (
     <>
-      <Header />
+      <Header openNewModalTransaction={openModal}/>
       <Cards />
       <Transaction />
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+      >
+        <h2>Testando Modal</h2>
+      </Modal>
       <GlobalStyles />
     </>
   );
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,13 +2,13 @@ import Logo from '../../assets/logo.svg';
 import { Component } from './styles';
 import { Content } from './styles';
 
-export function Header () {
+export function Header (props) {
 
     return (
         <Component>
             <Content>
                 <img src={Logo} alt="dt-money" />
-                <button type="button">
+                <button type="button" onClick={props.openNewModalTransaction}>
                     Nova Transação
                 </button>
             </Content>
